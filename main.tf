fea9a091512759853b2913d8ac461fbee0a7be09
Kolaganti8 <89938314+Kolaganti8@users.noreply.github.com>
2021-10-04 23:00:11 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,13 +3,13 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
       version = "3.44.0"
-    }
+      }
   }
   required_version = "> 0.14"
 }
 
 provider "aws" {
-  region = var.region
+  region = "eu-west-2"
 }
 
 resource "random_uuid" "randomid" {}
