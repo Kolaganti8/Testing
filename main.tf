0167743d5f3b9a5489d4a9eb9eb4f3192f7bd8af
Kolaganti8 <89938314+Kolaganti8@users.noreply.github.com>
2021-10-04 17:17:06 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = "eu-west-2"
+}
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+  backend "s3" {
+    bucket = "terraform-uni2"
+    key    = "platform.tfstate"
+    region = "eu-west-2"
+  }
+}
